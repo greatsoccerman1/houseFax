980c9866530b0570da20b37947189ea509e3aec6
Curtis <J.Ripley0805@gmail.com>
2021-12-09 14:43:19 -0600

    fixing some things

--- HouseFax/src/main/java/houseFaxRestClient/HouseFaxController.java
+++ HouseFax/src/main/java/houseFaxRestClient/HouseFaxController.java
@@ -1,9 +1,10 @@
-
+ 	
 package houseFaxRestClient;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
